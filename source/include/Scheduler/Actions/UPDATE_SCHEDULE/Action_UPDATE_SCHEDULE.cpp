f4fb6c8d523fc20a78cefca5cf081a26aaec12b8
mat-cab <mat-cab@users.noreply.github.com>
2017-10-25 18:57:42 -0300

    Implemented the UPDATE_SCHEDULE action

--- source/include/Scheduler/Actions/UPDATE_SCHEDULE/Action_UPDATE_SCHEDULE.cpp
+++ source/include/Scheduler/Actions/UPDATE_SCHEDULE/Action_UPDATE_SCHEDULE.cpp
@@ -1,5 +1,7 @@
 #include "../../../Debug_Lib.h"
 
+#include "../../../Wifi/Wifi.h"
+
 #include "Action_UPDATE_SCHEDULE.h"
 
 Action_UPDATE_SCHEDULE::Action_UPDATE_SCHEDULE(ActionType actionType, DaysMask dMask, uint8_t hour, uint8_t minute, uint8_t second) : Action(actionType, dMask, hour, minute, second) {
@@ -11,7 +13,11 @@ Action_UPDATE_SCHEDULE::Action_UPDATE_SCHEDULE(TimeMask *tMask, ActionMask *aMas
 }
 
 void Action_UPDATE_SCHEDULE::runAction() const {
-  debug("Sending the UPDATE_SCHEDULE command!");
+  // First connect to the Wifi
+  connectToWifi();
+
+  // Then get the schedule from the server
+  receiveWifi();
 }
 
 void Action_UPDATE_SCHEDULE::addAdditionalActionData(char * argument, uint8_t argumentNumber) const {
