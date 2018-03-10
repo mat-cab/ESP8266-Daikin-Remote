c2441ae56e1df60a90f86dcdac826de25db44465
mat-cab <mat-cab@users.noreply.github.com>
2018-03-10 16:53:57 -0300

    Changed debug mode activation
    Debug mode is no longer defined at compile time, it can be changed through pin 4

--- source/include/Debug_Lib.cpp
+++ source/include/Debug_Lib.cpp
@@ -2,42 +2,50 @@
 #include "Time.h"
 #include "Debug_Lib.h"
 
+boolean debug_mode = false;
+
 void initializeDebug() {
-#ifdef DEBUG
-  // Setup serial
-  Serial.begin(DEBUG_SPEED);
-  Serial.println();
-  Serial.println("ESP8266 starting - Time is "+String(weekday())+" "+String(day())+"/"+String(month())+"/"+String(year())+" "+String(hour())+":"+String(minute())+":"+String(second()));
-#endif
+  // Set the Debug pin as input
+  pinMode(DEBUG_PIN, INPUT);
+
+  // check if debug mode is activated on Debug pin
+  debug_mode = (digitalRead(DEBUG_PIN) == LOW);
+
+  if (debug_mode) {
+    // Setup serial
+    Serial.begin(DEBUG_SPEED);
+    Serial.println();
+    Serial.println("ESP8266 starting - Time is "+String(weekday())+" "+String(day())+"/"+String(month())+"/"+String(year())+" "+String(hour())+":"+String(minute())+":"+String(second()));
+  }
 }
 
 void endDebug() {
-#ifdef DEBUG
-  Serial.flush();
+  if (debug_mode) {
+    Serial.flush();
 
-  Serial.end();
-#endif
+    Serial.end();
+  }
 }
 
 void debug(String message) {
-#ifdef DEBUG
-  Serial.println(message);
-#endif
+  if (debug_mode) {
+    Serial.println(message);
+  }
 }
 
 void debug(struct measurement *measure) {
-#ifdef DEBUG
-  char fConverter[10]; 
+  if (debug_mode) {
+    char fConverter[10]; 
   
-  dtostrf(getTemperature(measure), 0, 2, fConverter);
-  debug( "Temperature : "+String(fConverter)+" degC");
+    dtostrf(getTemperature(measure), 0, 2, fConverter);
+    debug( "Temperature : "+String(fConverter)+" degC");
 
-  dtostrf(getHumidity(measure), 0, 2, fConverter);
-  Serial.println("Humidity : "+String(fConverter)+" %HR");  
+    dtostrf(getHumidity(measure), 0, 2, fConverter);
+    Serial.println("Humidity : "+String(fConverter)+" %HR");  
 
-  dtostrf(getVoltage(measure), 0, 3, fConverter);
-  Serial.println("Battery voltage : "+String(fConverter)+" V");
+    dtostrf(getVoltage(measure), 0, 3, fConverter);
+    Serial.println("Battery voltage : "+String(fConverter)+" V");
 
-  Serial.println("Delta with last measure : "+String( getDelta(measure) )+" s");
-#endif
+    Serial.println("Delta with last measure : "+String( getDelta(measure) )+" s");
+  }
 }
--- source/include/Debug_Lib.h
+++ source/include/Debug_Lib.h
@@ -2,9 +2,10 @@
 #define DEBUG_H_
 
 // Used for debug purposes
-// Comment for production
-//#define DEBUG
+// Speed for the serial connection
 #define DEBUG_SPEED 115200
+// GPIO pin for activating/deactivating the DEBUG
+#define DEBUG_PIN 4
 
 #include "Arduino.h"
 #include "Measurement.h"
