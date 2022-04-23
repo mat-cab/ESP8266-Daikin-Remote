0f2853c935d9abb08863b3357cceaad38a522023
mat-cab <mat-cab@users.noreply.github.com>
2022-04-22 23:04:41 -0300

    Added Wifi Customization for static IP Address

--- source/include/CustomConstants.h
+++ source/include/CustomConstants.h
@@ -17,6 +17,17 @@
 #define WIFI_SSID "WIFI_SSI"
 // Wifi Password
 #define WIFI_PASSWD "WIFI_PASSWORD"
+// State if Static IP must be used on this Wifi (or let DHCP decide everything)
+// Leave line commented if using DHCP
+//#define WIFI_USE_STATIC_IP
+// Wifi private static IP address (if WIFI_USE_STATIC_IP is defined)
+#define WIFI_IP IPAddress(192,168,1,2)
+// Wifi Gateway (if WIFI_USE_STATIC_IP is defined)
+#define WIFI_GATEWAY IPAddress(192,168,1,1)
+// Wifi Subnet (if WIFI_USE_STATIC_IP is defined)
+#define WIFI_SUBNET IPAddress(255,255,255,0)
+// Wifi name of the station
+#define WIFI_NAME String("ESP8266-NAME")
 
 // Private constants for Thingspeak channel
 // ID of the channel
